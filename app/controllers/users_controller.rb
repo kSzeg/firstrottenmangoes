1fc7220cd80158519cf38ba738545da9aff2624f
Kristina Szegidewicz <kristinamaryla@gmail.com>
2014-11-26 21:21:34 -0800

    exercise 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
+
 		if @user.save
-			session[:user_id] = @user.id # auto log in
-			redirect_to movies_path
+			session[:user_id] = @user.id
+			redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
 		else
 			render :new
 		end
