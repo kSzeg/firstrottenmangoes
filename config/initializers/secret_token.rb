c144bdc75d12f1f4a03013c8145bba2c2bf0e4e1
Kristina Szegidewicz <kristinamaryla@gmail.com>
2014-11-26 20:03:48 -0800

    Rotten mangoes version 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '362a705bf6a6c8199436c39b8a511c6f62e544a136b3134a4e994c9a2cd780948c407b5bb686e82a8a843cf4aab0d9fe32e3503dc9811db8c0dd2c9e1e3d35ec'
